4191b47968fce314198685ac804730e5b6c4e818
bloomluxesalon <bloomluxesalon@gmail.com>
2026-05-24 13:17:04 +0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,23 @@
-import { initializeApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB64nDJiEPS-JQJwBm3AtLaXJNsXQqnNk0',
-  authDomain: 'bloom-luxe-test.firebaseapp.com',
-  projectId: 'bloom-luxe-test',
-  storageBucket: 'bloom-luxe-test.firebasestorage.app',
-  messagingSenderId: '381744615197',
-  appId: '1:381744615197:web:e1c719348ba2d4d53a5bf4',
-  measurementId: 'G-NKQ235VRP6',
+  apiKey: 'AIzaSyD9MxQNhtlwMjT7u7LuXDxm98r4TzyCQNc',
+  authDomain: 'bloom-db-ec1b6.firebaseapp.com',
+  projectId: 'bloom-db-ec1b6',
+  storageBucket: 'bloom-db-ec1b6.firebasestorage.app',
+  messagingSenderId: '195797259146',
+  appId: '1:195797259146:web:b9f188227ad7e5217b6e73',
+  measurementId: 'G-08ST5Q9BGL',
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
 export { app, analytics };
